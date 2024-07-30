2e3a2fc9f7b1d089c261ae5c7a91911186eb8690
Pranjali <pranjali.j13@gmail.com>
2024-07-30 13:59:41 -0400

    Project Completion

---
+++ users_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from users_app.forms import CustomRegisterForm
+from django.contrib import messages
+from django.contrib.auth import logout
+# Create your views here.
+def register(request):
+    
+    if request.method=="POST":
+        register_form=CustomRegisterForm(request.POST)
+        
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, ("New user account created, Login to get started"))
+            return redirect('register')
+    else:
+        register_form =  CustomRegisterForm()
+    
+    return render(request, 'register.html', {'register_form':register_form})
+
+def custom_logout(request):
+    logout(request)
+    return render(request, 'logout.html', {})
